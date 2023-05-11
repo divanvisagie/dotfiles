b818819ca4050a60714a2c7e1fd0514bc74d27fe
Divan Visagie <me@dvisagie.com>
2023-05-11 17:06:19 +0200

    dark and light mode in vim

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,12 +1,20 @@
 function ColorMyPencils(color)
-    -- vim.o.background = "light"
+    vim.o.background = "light"
 	color = color or "rose-pine"
     -- color = color or "everforest"
-    vim.o.background = "dark"
+    -- vim.o.background = "dark"
 	vim.cmd.colorscheme(color)
 	-- Set background transparent
 	-- vim.api.nvim_set_hl(0, "NormalFloat",{ bg = "none" })
 	-- vim.api.nvim_set_hl(0, "Normal",{ bg = "none" })
+end
+
+function Dark()
+    vim.o.background = "dark"
+end
+
+function Light()
+    vim.o.background = "light"
 end
 
 ColorMyPencils()
--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -43,7 +43,7 @@ lsp.on_attach(function(client, bufnr)
 		  vim.lsp.buf.definition()
 		  vim.cmd("vsplit")
   end, opts)
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts) -- Go to definition
   vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
