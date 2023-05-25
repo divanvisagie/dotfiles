72b7f2707769314af620efe01ed16ea9117f7253
Divan visagie <me@dvisagie.com>
2023-05-25 21:22:39 +0200

    Multi lingual test solution

---
+++ nvim/after/plugin/test.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_keymap('n', '<leader>tf', ':TestFile<CR>', {noremap=true, silent=true})
+vim.api.nvim_set_keymap('n', '<leader>tn', ':TestNearest<CR>', {noremap=true, silent=true})
+
