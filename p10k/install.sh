18c599f155b3a3dfd3181f71ffa113424c1c0d5b
Divan visagie <me@dvisagie.com>
2023-06-02 15:57:37 +0200

    delete unnecessary

--- p10k/install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cp ~/dotfiles/p10k/.p10k.zsh ~
--- steal.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd ~
-
-cp .zshrc ~/dotfiles/zsh
-cp .tmux.conf ~/dotfiles/tmux
-cp .config/nvim/init.vim ~/dotfiles/nvim
-cp .config/alacritty/alacritty.yml ~/dotfiles/alacritty
-cp .p10k.zsh ~/dotfiles/p10k
-cp -R .config/i3 ~/dotfiles/
