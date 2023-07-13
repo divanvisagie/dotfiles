8b9bf392a114e2e283b968b761b0dcd42ffd990f
Divan Visagie <me@dvisagie.com>
2023-07-13 11:43:21 +0200

    Remove JS Dap which was causing errors

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -1,23 +1,3 @@
-require("dap").adapters["pwa-node"] = {
-  type = "server",
-  host = "localhost",
-  port = "${port}",
-  executable = {
-    command = "node",
-    -- 💀 Make sure to update this path to point to your installation
-    args = {"/path/to/js-debug/src/dapDebugServer.js", "${port}"},
-  }
-}
-require("dap").configurations.javascript = {
-  {
-    type = "pwa-node",
-    request = "attach",
-    name = "Attach Debugger",
-    program = "${file}",
-    cwd = "${workspaceFolder}",
-  },
-}
-
 -- Golang
 require('dap-go').setup()
 
