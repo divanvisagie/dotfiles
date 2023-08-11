1feaba204725dc390c681002747e17e137f2e307
Divan Visagie <me@dvisagie.com>
2023-08-11 17:33:49 +0200

    packer update

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -1,14 +1,24 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
+local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
     use("theprimeagen/refactoring.nvim")
     use("theprimeagen/harpoon")
 
     use "preservim/nerdtree"
     use 'simrat39/rust-tools.nvim'
 
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         requires = { {'nvim-lua/plenary.nvim'} }
