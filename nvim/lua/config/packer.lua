40824c5515d7648196a88350f323d251d5d5e856
Divan visagie <me@dvisagie.com>
2023-08-26 15:20:54 +0200

    Indentation

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -125,7 +125,7 @@ return require('packer').startup(function(use)
     {'hrsh7th/nvim-cmp'},     -- Required
     {'hrsh7th/cmp-nvim-lsp'}, -- Required
     {'L3MON4D3/LuaSnip'},     -- Required
+  }
 }
-        }
 
-    end)
+end)
