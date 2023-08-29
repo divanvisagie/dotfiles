9eb356fb404ef1bb919b1067b834e8e8d09ccc5f
Divan visagie <me@dvisagie.com>
2023-08-29 20:20:20 +0200

    wallpaper switching

--- scripts/darkmode.sh
+++ scripts/darkmode.sh
@@ -6,10 +6,12 @@ alacritty_config=$(realpath ~/.dotfiles/alacritty/alacritty.yml)
 if [ $current_mode == "true" ]; then
     yq eval 'del(.import[]) | .import = ["~/.config/alacritty/themes/themes/rose-pine-dawn.yaml"]' -i "$alacritty_config" 
     osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to false'
+    wallpaper set ~/.dotfiles/wallpapers/light.jpg
     echo "Switched to light mode."
 else
     yq eval 'del(.import[]) | .import = ["~/.config/alacritty/themes/themes/everforest_dark.yaml"]' -i "$alacritty_config" 
     osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
+    wallpaper set ~/.dotfiles/wallpapers/dark.png
     echo "Switched to dark mode."
 fi
 
