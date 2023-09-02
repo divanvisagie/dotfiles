7f5b25b2e1c822e3cbc1b26d534d0e9fc802f694
Divan Visagie <me@dvisagie.com>
2023-09-02 08:51:50 +0200

    no more ctrl p for telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles'})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = '[G]it Files'})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string( { search = vim.fn.input("Grep > ") });
-end)
+end, { desc = '[F]ind grep [S]tring'})
