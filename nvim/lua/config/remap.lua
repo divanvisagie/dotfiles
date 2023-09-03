68bc29b943e3eba6326ea7118b2d328b77e0d124
Divan visagie <me@dvisagie.com>
2023-09-03 17:29:29 +0200

    Cleaner remaps

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,4 +1,6 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Allows you to move the selected lines up or down
@@ -9,7 +11,7 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.api.nvim_set_keymap('n', '<leader>%', ':vsplit<CR>', {noremap = true, silent = true}) -- vertical split
 vim.api.nvim_set_keymap('n', '<leader>"', ':split<CR>', {noremap = true, silent = true}) -- horizontal split
 
-
+-- go to previous buffer
 vim.api.nvim_set_keymap('n', '<leader>bp', ':bp<CR>', {noremap = true, silent = true})
 
 
