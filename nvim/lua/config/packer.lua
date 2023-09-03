1dd180bb15ef626b4127a8c1ddabd43e89c7bde0
Divan visagie <me@dvisagie.com>
2023-09-03 17:56:32 +0200

    Cleaner packer file

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -19,53 +19,46 @@ return require('packer').startup(function(use)
     use 'preservim/nerdtree'
 
     use 'simrat39/rust-tools.nvim'
+    use 'nvim-tree/nvim-web-devicons'
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
     -- Display errors better
-    use { "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    }
+    use 'folke/trouble.nvim'
+
     -- copilot
-    use {
-        "zbirenbaum/copilot.lua",
-    }
+    use 'zbirenbaum/copilot.lua'
+
     -- debug adapter protocol
     use 'mfussenegger/nvim-dap'
-    use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
+    use 'rcarriga/nvim-dap-ui'
     use 'leoluz/nvim-dap-go'
     -- run tests
     use {
         "klen/nvim-test"
     }
-    -- Status line
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
-    use { 'anuvyklack/pretty-fold.nvim' }
-    -- guess indentation
-    use { 'nmac427/guess-indent.nvim' }
-    -- ColorScheme
-    -- If you are using Packer
-    use { "ellisonleao/gruvbox.nvim" }
+    use 'nvim-lualine/lualine.nvim' -- Better status line
+
+    use 'anuvyklack/pretty-fold.nvim' -- Better code folding
+    use 'nmac427/guess-indent.nvim' -- guess indentation
+
+    -- Color Schemes
     use({ 'rose-pine/neovim', as = 'rose-pine' })
-    use { "sainnhe/everforest" }
-    -- Better Syntax Highlighting
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    -- A history of your undos
-    use('mbbill/undotree')
-    -- File tree
-    use('nvim-tree/nvim-web-devicons')
-    -- Comments
-    use { 'numToStr/Comment.nvim' }
+    use 'sainnhe/everforest'
+
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }) -- Better Syntax Highlighting
+
+    use 'mbbill/undotree'  -- A history of your undos
+    use { 'numToStr/Comment.nvim' } -- Easier code comments
+
     -- Shows shortcut key help when initiating a command
     use { "folke/which-key.nvim" }
 
     -- helpers for neovim lua
     use { "folke/neodev.nvim" }
+
     -- lsp
     use {
         'VonHeikemen/lsp-zero.nvim',
