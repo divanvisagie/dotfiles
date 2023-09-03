ef086a92e200e590b06d96a69f026a4857510ddf
Divan visagie <me@dvisagie.com>
2023-09-03 17:31:40 +0200

    Nerdtree find

--- nvim/after/plugin/nerdtree.lua
+++ nvim/after/plugin/nerdtree.lua
@@ -1,2 +1,5 @@
 -- NERDTree toggle mapping
 vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTreeToggle<CR>', {noremap=true, silent=true})
+
+-- Mapping to find the active file in NERDTree
+vim.api.nvim_set_keymap('n', '<leader>haf', ':NERDTreeFind<CR>', {noremap = true, silent = true})
