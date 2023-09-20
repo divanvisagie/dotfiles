7146b2679fb27461261687b38ebca07414df8135
Divan Visagie <me@dvisagie.com>
2023-09-20 16:47:46 +0200

    Comment out buffer previous because it conflicts and is not needed

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -7,12 +7,11 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
-
 vim.api.nvim_set_keymap('n', '<leader>%', ':vsplit<CR>', {noremap = true, silent = true}) -- vertical split
 vim.api.nvim_set_keymap('n', '<leader>"', ':split<CR>', {noremap = true, silent = true}) -- horizontal split
 
 -- go to previous buffer
-vim.api.nvim_set_keymap('n', '<leader>bp', ':bp<CR>', {noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('n', '<leader>bp', ':bp<CR>', {noremap = true, silent = true})
 
 
 -- Toggle line numbers in context
