30f28b65ff1bd959a5d6d6214de69c1af140504b
Divan Visagie <me@divanv.com>
2023-09-30 12:58:42 +0200

    Neovim clipboard more hardcore on linux

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -8,7 +8,7 @@ vim.opt.smartindent = true
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "80"
 vim.opt.scrolloff = 8
-vim.opt.clipboard = "unnamed"
+vim.opt.clipboard = "unnamedplus"
 vim.opt.undolevels = 10000
 
 vim.o.termguicolors = true
