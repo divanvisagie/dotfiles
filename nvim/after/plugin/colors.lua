a705559ea098137b4068bbca20fea426b0ed3943
Divan Visagie <visagiedivan@gmail.com>
2024-03-03 15:24:03 +0100

    Cross platform dark checking
    Moved the nvim dark mode check to a cross platform implementaiton

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -7,7 +7,6 @@ end
 function Dark()
     vim.o.background = "dark"
 	vim.cmd.colorscheme("everforest")
-    Trans()
 end
 
 function Light()
@@ -15,9 +14,25 @@ function Light()
 	vim.cmd.colorscheme("rose-pine")
 end
 
-local function isDarkModeEnabled()
+local function isDarkModeEnabledMac()
     local darkModeOutput = os.execute("defaults read -g AppleInterfaceStyle 2>/dev/null")
     return darkModeOutput == 0
+end
+
+local function isDarkModeEnabledLinux()
+    local darkModeOutput = os.execute("gsettings get org.gnome.desktop.interface gtk-theme 2>/dev/null")
+    return darkModeOutput == 0
+end
+
+local function isDarkModeEnabled()
+    -- generic check for mac and linux
+    -- first checkt if the os is mac
+    if vim.fn.has("mac") == 1 then
+        return isDarkModeEnabledMac()
+    else
+        return isDarkModeEnabledLinux()
+    end
+    return true
 end
 
 if isDarkModeEnabled() then
