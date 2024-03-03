be17f22e0fddf288ec2dcf98bd04fc1f9498d3cb
Divan visagie <me@dvisagie.com>
2024-03-03 15:46:01 +0100

    Get dark mode back on macos

--- scripts/darkmode.sh
+++ scripts/darkmode.sh
@@ -2,16 +2,19 @@
 
 # Toggle dark mode
 current_mode=$(osascript -e 'tell application "System Events" to tell appearance preferences to return dark mode')
-alacritty_config=$(realpath ~/.dotfiles/alacritty/alacritty.yml)
-if [ $current_mode == "true" ]; then
-    yq eval 'del(.import[]) | .import = ["~/.config/alacritty/themes/themes/rose-pine-dawn.yaml"]' -i "$alacritty_config" 
+alacritty_config=$(realpath ~/.dotfiles/alacritty/alacritty.toml) # Update this path to your alacritty config location if needed
+
+light_theme="~/.config/alacritty/themes/themes/rose-pine-dawn.toml"
+dark_theme="~/.config/alacritty/themes/themes/everforest_dark.toml"
+
+if [ "$current_mode" == "true" ]; then
+    sed -i '' "s|import = .*|import = [\"$light_theme\"]|" "$alacritty_config"
     osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to false'
     wallpaper set ~/.dotfiles/wallpapers/light.jpg
     echo "Switched to light mode."
 else
-    yq eval 'del(.import[]) | .import = ["~/.config/alacritty/themes/themes/everforest_dark.yaml"]' -i "$alacritty_config" 
+    sed -i '' "s|import = .*|import = [\"$dark_theme\"]|" "$alacritty_config"
     osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
     wallpaper set ~/.dotfiles/wallpapers/dark.png
     echo "Switched to dark mode."
 fi
-
