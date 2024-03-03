b340efd038213183f1c30d424b43578b9dc4e40b
Divan visagie <me@dvisagie.com>
2024-03-03 14:59:46 +0100

    Update alacritty installation

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,1 +1,11 @@
 ln -s ~/.dotfiles/alacritty ~/.config/alacritty
+
+# Clone the alacritty repo if it does not exist
+if [ ! -d ~/Projects/com.github/alacritty/alacritty ]; then
+	mkdir ~/Projects/com.github/alacritty
+	cd ~/Projects/com.github/alacritty
+	git clone https://github.com/alacritty/alacritty.git
+	cd alacritty
+	make install
+	cd ~
+fi
