88b71283344794e3694119afdf42968e7bdd25cf
Divan Visagie <visagiedivan@gmail.com>
2024-03-23 08:15:40 +0100

    Remove unused

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,7 +9,6 @@ export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
-source $ZSH/oh-my-zsh.sh
 source $ZSH/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 plugins=(git history zsh-autosuggestions)
 
