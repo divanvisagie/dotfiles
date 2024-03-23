2f03f3bf52c80a8b80cf32f244e16144a66ea963
Divan Visagie <me@divanv.com>
2024-03-23 07:32:59 +0100

    Better which key documentation

--- nvim/after/plugin/blame.lua
+++ nvim/after/plugin/blame.lua
@@ -2,3 +2,14 @@ require('gitblame').setup {
      --Note how the `gitblame_` prefix is omitted in `setup`
     enabled = false,
 }
+
+
+local wk = require("which-key")
+local mappings = {
+    g = {
+        name = "Git",
+        b = { "<cmd>Git blame<CR>", "Blame" },
+    },
+}
+local opts = { prefix = "<leader>" }
+wk.register(mappings, opts)
--- nvim/after/plugin/nerdtree.lua
+++ nvim/after/plugin/nerdtree.lua
@@ -4,3 +4,14 @@ vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTreeToggle<CR>', {noremap=true, sile
 -- Mapping to find the active file in NERDTree
 vim.api.nvim_set_keymap('n', '<leader>haf', ':NERDTreeFind<CR>', {noremap = true, silent = true})
 vim.g.NERDTreeShowHidden = 1
+
+local wk = require("which-key")
+local mappings = {
+	n = {
+		name = "NERDTree",
+		t = { "<cmd>NERDTreeToggle<CR>", "Toggle" },
+		f = { "<cmd>NERDTreeFind<CR>", "Find" },
+	},
+}
+local opts = { prefix = "<leader>" }
+wk.register(mappings, opts)
