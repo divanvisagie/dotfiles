5c7f4058733c424e5d5c53eabbd402f7f4446556
Divan Visagie <me@divanv.com>
2024-04-10 11:57:09 +0200

    Update refactorings shortcuts

--- nvim/after/plugin/refactoring.lua
+++ nvim/after/plugin/refactoring.lua
@@ -1,10 +1,11 @@
-vim.keymap.set("x", "<leader>re", ":Refactor extract ", { desc = "Refactor extract" })
-vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
+require('refactoring').setup({})
+vim.keymap.set("x", "<leader>ree", ":Refactor extract ", { desc = "Refactor extract" })
+vim.keymap.set("x", "<leader>ref", ":Refactor extract_to_file ")
 
 vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
 
-vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
+vim.keymap.set({ "n", "x" }, "<leader>riv", ":Refactor inline_var")
+vim.keymap.set( "n", "<leader>rif", ":Refactor inline_func")
 
 vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
 vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
-
