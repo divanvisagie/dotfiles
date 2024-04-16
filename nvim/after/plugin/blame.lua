a0801f5fa16046ef716bc3aedf0200acd3bcbba0
Divan Visagie <me@divanv.com>
2024-04-16 11:18:28 +0200

    Update refactoring mappings

--- nvim/after/plugin/blame.lua
+++ nvim/after/plugin/blame.lua
@@ -2,14 +2,3 @@ require('gitblame').setup {
      --Note how the `gitblame_` prefix is omitted in `setup`
     enabled = false,
 }
-
-
-local wk = require("which-key")
-local mappings = {
-    g = {
-        name = "Git",
-        b = { "<cmd>Git blame<CR>", "Blame" },
-    },
-}
-local opts = { prefix = "<leader>" }
-wk.register(mappings, opts)
--- nvim/after/plugin/refactoring.lua
+++ nvim/after/plugin/refactoring.lua
@@ -1,11 +1,12 @@
 require('refactoring').setup({})
-vim.keymap.set("x", "<leader>ree", ":Refactor extract ", { desc = "Refactor extract" })
-vim.keymap.set("x", "<leader>ref", ":Refactor extract_to_file ")
+vim.keymap.set("n", "<leader>ree", ":Refactor extract ", { desc = "Extract" })
+vim.keymap.set("n", "<leader>ref", ":Refactor extract_to_file", { desc = "Extract to file"})
 
-vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
+vim.keymap.set("n", "<leader>rev", ":Refactor extract_var")
 
-vim.keymap.set({ "n", "x" }, "<leader>riv", ":Refactor inline_var")
+vim.keymap.set("n", "<leader>riv", ":Refactor inline_var")
 vim.keymap.set( "n", "<leader>rif", ":Refactor inline_func")
 
 vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
 vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
+
