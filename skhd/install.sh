e2443523f197f15907652d046a2e1d480dc6c381
Divan Visagie <me@divanv.com>
2024-04-19 23:41:21 +0200

    Make installer more robust

--- skhd/install.sh
+++ skhd/install.sh
@@ -1,1 +1,10 @@
+# create if does not exist
+if [ ! -d ~/.config ]; then
+	mkdir ~/.config
+fi
+
+if [ ! -d ~/.config/skhd ]; then
+	mkdir ~/.config/skhd
+fi
+
 ln -s ~/.dotfiles/skhd/.skhdrc ~/.skhdrc
