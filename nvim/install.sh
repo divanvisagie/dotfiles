40de6f73632ac0f2d2e06dd0a8d1eeee38b4f9bc
Divan Visagie <me@divanv.com>
2024-04-21 00:37:16 +0200

    Update neovim install

--- nvim/install.sh
+++ nvim/install.sh
@@ -5,13 +5,13 @@ if [ ! -d ~/Projects/com.github/neovim/neovim ]; then
 	mkdir ~/Projects/com.github/neovim
 	cd ~/Projects/com.github/neovim
 	git clone git@github.com:neovim/neovim.git
-	cd neovim
-	
-	## only build if nvim not present
-	if ! [ -x "$(command -v nvim)" ]; then
-		make CMAKE_BUILD_TYPE=Release
-		make install
-	fi
-	cd ~
 fi
+
+cd ~/Projects/com.github/neovim/neovim
+## only build if nvim not present
+if ! [ -x "$(command -v nvim)" ]; then
+	make CMAKE_BUILD_TYPE=Release
+	sudo make install
+fi
+cd ~
 
