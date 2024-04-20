7c5980f5e296eb7c929486b4f7aee48c5212186d
Divan Visagie <me@divanv.com>
2024-04-21 00:36:39 +0200

    update alacritty install

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -5,11 +5,15 @@ if [ ! -d ~/Projects/com.github/alacritty/alacritty ]; then
 	mkdir ~/Projects/com.github/alacritty
 	cd ~/Projects/com.github/alacritty
 	git clone https://github.com/alacritty/alacritty.git
-	cd alacritty
-
-	## only build if alacritty command not present
-	if ! [ -x "$(command -v alacritty)" ]; then
-		cargo install --path .
-	fi
-	cd ~
 fi
+
+cd ~/Projects/com.github/alacritty/alacritty
+## only build if alacritty command not present
+if ! [ -x "$(command -v alacritty)" ]; then
+	cargo build --release
+	sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+	sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+	sudo desktop-file-install extra/linux/Alacritty.desktop
+	sudo update-desktop-database
+fi
+cd ~
