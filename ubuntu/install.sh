a253faffe6ad931d5ea8c0e08f5157669131da05
Divan Visagie <me@divanv.com>
2024-04-24 17:29:17 +0200

    Update ubuntu installer

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt update
+sudo apt upgrade
 sudo apt install avahi-daemon -y # install mdns 
 sudo apt install build-essential -y
 sudo apt install curl git -y
@@ -15,10 +17,15 @@ sudo apt install librust-servo-fontconfig-sys-dev -y
 sudo apt install ninja-build -y
 sudo apt install gnome-tweaks -y
 sudo apt install ninja-build gettext cmake unzip curl build-essential -y #needed for neovim build
+sudo apt install libssl-dev -y #needed for rust dev 
+sudo apt install libglib2.0-dev -y
+sudo apt install psensor 0y
 
 # install zsh and set it as the default shell
 sudo apt install zsh -y
 chsh -s $(which zsh)
 
-
-
+# set up flatpak
+sudo apt install flatpak -y
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.github.d4nj1.tlpui
