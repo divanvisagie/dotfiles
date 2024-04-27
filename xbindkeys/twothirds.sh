a837a6808497772d2cc4ee311f9a4f8cd085197c
Divan Visagie <visagiedivan@gmail.com>
2024-04-27 09:23:58 +0200

    Iterations on window management
    Added two thirds mapping option to be same as rectangle and made center not always vert max

--- xbindkeys/twothirds.sh
+++ xbindkeys/twothirds.sh
@@ -9,21 +9,22 @@ ACTIVE_WIN_ID=$(xdotool getactivewindow)
 
 # Unmaximize the window to allow moving it
 wmctrl -i -r $ACTIVE_WIN_ID -b remove,maximized_vert,maximized_horz
-# wmctrl -i -r $ACTIVE_WIN_ID -b remove,maximized_horz
 
 # Wait a bit for the window manager to process the unmaximize command
 sleep 0.1
 
-# Get active window width and height
-WIN_WIDTH=$(xwininfo -id $ACTIVE_WIN_ID | grep 'Width:' | awk '{print $2}')
-WIN_HEIGHT=$(xwininfo -id $ACTIVE_WIN_ID | grep 'Height:' | awk '{print $2}')
+## Calculate thirds
+THIRD=$(($SCREEN_WIDTH / 3))
+
+NEW_HEIGHT=$(($SCREEN_HEIGHT - 90))
+NEW_WIDTH=$(($SCREEN_WIDTH * 2 / 3))
 
 # Calculate new window position to center it
-NEW_X=$((($SCREEN_WIDTH - $WIN_WIDTH) / 2))
-NEW_Y=$((($SCREEN_HEIGHT - $WIN_HEIGHT) / 2))
+NEW_X=0
+NEW_Y=$((($SCREEN_HEIGHT - $NEW_HEIGHT) / 2))
 
 # Move the active window to the center of the screen
-wmctrl -i -r $ACTIVE_WIN_ID -e 0,$NEW_X,$NEW_Y,$WIN_WIDTH,$WIN_HEIGHT
+wmctrl -i -r $ACTIVE_WIN_ID -e 0,$NEW_X,$NEW_Y,$NEW_WIDTH,$NEW_HEIGHT
 
 # Optional: Re-maximize the window if needed
-#wmctrl -i -r $ACTIVE_WIN_ID -b add,maximized_vert
+wmctrl -i -r $ACTIVE_WIN_ID -b add,maximized_vert
