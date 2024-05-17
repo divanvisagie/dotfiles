f5dd00b2cb54b1350f4d396ec34ad4b156786b98
Divan Visagie <me@divanv.com>
2024-05-17 02:20:44 +0200

    Don't need that

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -73,6 +73,5 @@ function today() {
 }
 
 eval "$(direnv hook zsh)"
-source $ZSH/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
