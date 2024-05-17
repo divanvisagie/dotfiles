fa448da9417577a8c6786994de716afa2f9b6b97
Divan Visagie <me@divanv.com>
2024-05-17 11:57:04 +0200

    Switch to starship prompt in installer

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,12 +5,8 @@ if [ ! -f ~/.zshrc ]; then
 	ln -s ~/.dotfiles/zsh/ ~/.zsh
 fi
 
-# Zsh setup
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Starship prompt
+curl -sS https://starship.rs/install.sh | sh
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	sh ./ubuntu/install.sh
