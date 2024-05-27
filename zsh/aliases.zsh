102831e8bd714a832367449148408d99aa33b748
Divan Visagie <me@divanv.com>
2024-05-28 01:43:15 +0200

    add yazi shortcut

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -104,6 +104,15 @@ function today() {
     fi
 }
 
+function yz() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+
 # Shell integrations
 eval "$(direnv hook zsh)"
 
