88b0c1fbc4a5ba72b0851221371236e7b4b059cf
Divan Visagie <me@divanv.com>
2024-06-12 23:00:29 +0200

    Add csep to nvim

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -10,3 +10,34 @@ vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = '[G]it Files'})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string( { search = vim.fn.input("Grep > ") });
 end, { desc = '[F]ind grep [S]tring'})
+
+vim.keymap.set('n', '<leader>cs', function()
+  builtin.find_files( { 
+    find_command = {'csep', '--vimgrep'},
+  });
+end, { desc = '[C]sep'})
+
+
+local function Csep(pattern)
+    -- Construct the ripgrep command
+    local command = 'csep --vimgrep ' .. vim.fn.shellescape(pattern)
+    
+    -- Capture the output of the command
+    local output = vim.fn.systemlist(command)
+    
+    -- Set the quickfix list with the command output
+    vim.fn.setqflist({}, 'r', { title = 'Csep Search', lines = output })
+    
+    -- Open the quickfix window
+    vim.cmd('copen')
+end
+
+-- Create a key mapping for the function call
+vim.keymap.set('n', '<leader>cs', function()
+    -- Prompt the user for a search pattern
+    local pattern = vim.fn.input('Csep > ')
+    
+    -- Call the function with the search pattern
+    Csep(pattern)
+end, { desc = '[C]sep'})
+
