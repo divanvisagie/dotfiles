c9e6bceccf6058d321bc5487aa4c47e1c89a49bc
Divan Visagie <me@divanv.com>
2024-06-20 23:46:00 +0200

    Build a debugger the hard way

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -48,12 +48,27 @@ vim.api.nvim_create_autocmd('FileType', {
 -- Create an autocommand group for TypeScript file bindings
 vim.api.nvim_create_augroup('TypeScriptBindings', { clear = true })
 
+function Insert_console_log()
+  -- Get current line number and file name
+  local line_num = vim.fn.line('.')
+  local file_name = vim.fn.expand('%:p')
+  --- get relative path to file
+  file_name = vim.fn.fnamemodify(file_name, ':~:.')
+
+  -- Construct the console.log statement
+  local log_statement = string.format("console.log('BREAKPOINT', '%d:%s', this || '');", line_num+1, file_name)
+
+  -- Use nvim_buf_set_lines to insert the log statement
+  vim.api.nvim_buf_set_lines(0, line_num, line_num, false, { log_statement })
+end
 -- Define key mappings for TypeScript files
 vim.api.nvim_create_autocmd('FileType', {
   group = 'TypeScriptBindings',
   pattern = 'typescript',
   callback = function()
-    -- Set keybinding to insert console.log on the next line
-    vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>b', "oconsole.log('BREAKPOINT', this || 'no value');<ESC>", { noremap = true, silent = true })
+    -- Define a Lua function that inserts a console.log statement with line number and file name
+
+    -- Set keybinding to insert console.log on the next line and run the Lua function
+    vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>b', [[:lua Insert_console_log()<CR>]], { noremap = true, silent = true })
   end,
 })
