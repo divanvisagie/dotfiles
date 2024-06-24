6f1084698140ea7f00b4eb9722a896130425aed7
Divan Visagie <me@divanv.com>
2024-06-24 22:48:56 +0200

    Sort nix packages

--- nix/default.nix
+++ nix/default.nix
@@ -3,23 +3,24 @@
 pkgs.buildEnv {
   name = "my-environment";
   paths = [
+    pkgs.autoconf
+    pkgs.autogen
     pkgs.bun
     pkgs.clang
-    pkgs.autogen
-    pkgs.autoconf
     pkgs.cmake
+    pkgs.cowsay
+    pkgs.fastfetch
+    pkgs.fortune
+    pkgs.gh
     pkgs.go
+    pkgs.lolcat
     pkgs.neovim
+    pkgs.nix-search-cli
+    pkgs.nyancat
     pkgs.ocaml
     pkgs.odin
     pkgs.tmux
     pkgs.vim
     pkgs.zig
-    pkgs.fastfetch
-    pkgs.gh
-    pkgs.nix-search-cli
-    pkgs.cowsay
-    pkgs.lolcat
-    pkgs.fortune
   ];
 }
