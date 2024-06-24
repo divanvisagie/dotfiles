83039fa96af989457f1639ffaa4e240ba5d3bd3b
Divan Visagie <me@divanv.com>
2024-06-24 20:13:14 +0200

    Remove source installers
    Removes source installers for alacritty and neovim

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 set -e # The script will exit if it hits an error code
-if [ ! -d ~/.dotfiles/alacritty ]; then
+if [ ! -f ~/.dotfiles/alacritty ]; then
 	ln -s ~/.dotfiles/alacritty/ ~/.config/
 fi
-
-# Clone the alacritty repo if it does not exist
-if [ ! -d ~/Projects/github.com/alacritty/alacritty ]; then
-	mkdir ~/Projects/github.com/alacritty
-	cd ~/Projects/github.com/alacritty
-	git clone https://github.com/alacritty/alacritty.git
-fi
-
-cd ~/Projects/github.com/alacritty/alacritty
-## only build if alacritty command not present
-if ! [ -x "$(command -v alacritty)" ]; then
-	cargo build --release
-	sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
-	sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-	sudo desktop-file-install extra/linux/Alacritty.desktop
-	sudo update-desktop-database
-else 
-	if gum confirm "Do you want to update alacritty?"; then 
-		git pull
-		cargo build --release
-		sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
-		sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
-	fi
-fi
-cd ~/.dotfiles
--- nvim/install.sh
+++ nvim/install.sh
@@ -3,26 +3,3 @@ set -e # The script will exit if it hits an error code
 
 rm ~/.config/nvim
 ln -s ~/.dotfiles/nvim/ ~/.config/
-
-# Clone the nvim repo if it does not exist
-if [ ! -d ~/Projects/github.com/neovim/neovim ]; then
-	mkdir -p ~/Projects/github.com/neovim
-	cd ~/Projects/github.com/neovim
-	git clone git@github.com:neovim/neovim.git
-fi
-
-cd ~/Projects/github.com/neovim/neovim
-## only build if nvim not present
-if ! [ -x "$(command -v nvim)" ]; then
-	make CMAKE_BUILD_TYPE=Release
-	sudo make install
-else
-	# Update if we already have it
-	if gum confirm "Do you want to update nvim?"; then 
-		git pull
-		make CMAKE_BUILD_TYPE=Release
-		sudo make install
-	fi
-fi
-cd ~
-
