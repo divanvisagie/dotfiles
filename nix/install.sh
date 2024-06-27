64e058cf6622b28bd18c9ffb3139f2d108195e00
Divan Visagie <visagiedivan@gmail.com>
2024-06-27 21:56:49 +0200

    Allow unfree nix pkgs

--- nix/install.sh
+++ nix/install.sh
@@ -2,6 +2,8 @@
 
 # Move the configuration to the custom profile directory
 mkdir -p ~/.nix-profile-custom
-cp ~/.dotfiles/nix/default.nix ~/.nix-profile-custom/default.nix
-nix-env -i -f ~/.nix-profile-custom/default.nix
 
+cp ~/.dotfiles/nix/default.nix ~/.nix-profile-custom/default.nix
+cp config.nix ~/.config/nixpkgs/config.nix
+
+nix-env -i -f ~/.nix-profile-custom/default.nix
