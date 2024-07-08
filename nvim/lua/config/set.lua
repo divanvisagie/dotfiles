6f3d8e9de3762b3ea7dcbab87239c9c6e4833133
Divan Visagie <me@divanv.com>
2024-07-08 18:39:03 +0200

    Build Chatgpt scratchpad

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -37,64 +37,21 @@ vim.api.nvim_exec([[
   autocmd FileType markdown setlocal colorcolumn=0
 ]], false) 
 
-function GetVisualSelection()
-  local start_pos = vim.fn.getpos("'<")
-  local end_pos = vim.fn.getpos("'>")
+function ChatGipity()
+  local buf_name = "~/.cache/chat-gipity"
+  local existing_bufnr = vim.fn.bufnr(buf_name)
 
-  local start_line = start_pos[2]
-  local start_col = start_pos[3]
-  local end_line = end_pos[2]
-  local end_col = end_pos[3]
-
-  if start_line == end_line then
-    return vim.fn.getline(start_line):sub(start_col, end_col)
+  -- Check if the buffer is already open
+  if existing_bufnr == -1 then
+    -- Open new vertical split with the buffer
+    vim.cmd("vsplit " .. buf_name)
+    -- Set filetype to markdown for the new buffer
+    vim.bo.filetype = 'markdown'
   else
-    local lines = vim.fn.getline(start_line, end_line)
-    lines[1] = lines[1]:sub(start_col, -1)
-    lines[#lines] = lines[#lines]:sub(1, end_col)
-    return table.concat(lines, "\n")
+    -- Split and display the existing buffer
+    vim.cmd('vsplit')
+    vim.cmd('buffer ' .. existing_bufnr)
   end
 end
 
-function AddSelectionToQuickfix()
-  -- Obtain text from visual selection
-  local line = GetVisualSelection()
-  print(line)
-
-  -- The line is quickfix list compatible, we need to extract each value
-  local filename = ''
-  local line_num = 0
-  local body = ''
-
-  -- first we split values by : or space
-  local parts = vim.fn.split(line, '[: ]')
-
-  for part_idx, part in ipairs(parts) do
-    if part_idx == 1 then
-      filename = part
-    end
-    if part_idx == 2 then
-      line_num = tonumber(part)
-    end
-    if part_idx > 2 then
-        body = body .. part
-    end
-  end
-
-  local qf_list = {}
-  table.insert(qf_list, {
-    filename = filename,
-    lnum = line_num,  -- Line number
-    col = 1,                        -- Column number
-    text = body                     -- Text
-  })
-
-  vim.fn.setqflist(qf_list, 'a')  -- Append to the current quickfix list
-  if vim.fn.empty(vim.fn.getqflist()) == 0 then
-      vim.cmd('copen')
-  end
-end
-
-vim.api.nvim_set_keymap('v', '<leader>qf', ":lua AddSelectionToQuickfix()<CR>", { noremap = true, silent = true })
-
-
+vim.api.nvim_set_keymap('n', '<leader>cg', ':lua ChatGipity()<CR>', {noremap = true})
