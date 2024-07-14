e3bb479f24351c0682e9adf73483d72c03acca1c
Divan Visagie <visagiedivan@gmail.com>
2024-07-14 13:26:51 +0200

    Change some settings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -17,20 +17,10 @@ config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = false
 
 config.window_frame = {
-  -- The font used in the tab bar.
-  -- Roboto Bold is the default; this font is bundled
-  -- with wezterm.
-  -- Whatever font is selected here, it will have the
-  -- main font setting appended to it to pick up any
-  -- fallback fonts you may have used there.
   font = wezterm.font { family = 'Ubuntu', weight = 'Bold' },
 
   -- Default to 10.0 on Windows but 12.0 on other systems
-  font_size = 18.0,
-
-  -- The overall background color of the tab bar when
-  -- the window is not focused
-  inactive_titlebar_bg = '#333333',
+  font_size = 16.0,
 }
 
 -- Don't pad the window
