2694e589857f2a5e761116747e45a82dd9bbbe1f
Divan Visagie <me@divanv.com>
2024-09-06 15:02:47 +0200

    Update

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,41 +1,18 @@
+-- Require Telescope and its builtins
 require('telescope').setup()
+
 local builtin = require('telescope.builtin')
---vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles'})
+
+-- Key mappings for Telescope functions
 vim.keymap.set('n', '<leader>ff', function()
   builtin.find_files({
     find_command = {'rg', '--files', '--hidden', '--glob', '!.git/*'},
   })
-end, { desc = '[F]ind [F]iles'})
-vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = '[G]it Files'})
+end, { desc = '[F]ind [F]iles' })
+
+vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = '[G]it Files' })
 
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string( { search = vim.fn.input("Grep > ") });
-end, { desc = '[F]ind grep [S]tring'})
-
-
-local function Csep(pattern)
-    -- Construct the ripgrep command
-    local command = 'csep --vimgrep ' .. vim.fn.shellescape(pattern)
-    
-    -- Capture the output of the command
-    local output = vim.fn.systemlist(command)
-    
-    -- Set the quickfix list with the command output
-    vim.fn.setqflist({}, 'r', { title = 'Csep Search', lines = output })
-    
-    -- put it in telescope
-    -- builtin.quickfix({results = output, title = 'Csep Search'})
-    
-    -- Open the quickfix window
-    vim.cmd('copen')
-end
-
--- Create a key mapping for the function call
-vim.keymap.set('n', '<leader>cs', function()
-    -- Prompt the user for a search pattern
-    local pattern = vim.fn.input('Csep > ')
-    
-    -- Call the function with the search pattern
-    Csep(pattern)
-end, { desc = '[C]sep'})
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end, { desc = '[F]ind grep [S]tring' })
 
