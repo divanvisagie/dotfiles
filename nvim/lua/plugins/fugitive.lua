1b6649be84e05ee6d0b58a15d485f0cb861ae634
Divan Visagie <me@divanv.com>
2025-05-19 22:40:51 +0200

    add keymap descriptions and inline diff shortcut to fugitive config
    - Added "desc" field for all fugitive keymaps for better discoverability
    - Added <leader>gi for inline git diff in the current buffer

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,8 +1,13 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>G<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gs', '<cmd>Gvdiffsplit<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gb', '<cmd>Git blame<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>G<CR>',
+      { noremap = true, silent = true, desc = "Fugitive: Git status" })
+    vim.api.nvim_set_keymap('n', '<leader>gs', '<cmd>Gvdiffsplit<CR>',
+      { noremap = true, silent = true, desc = "Fugitive: Git diff split" })
+    vim.api.nvim_set_keymap('n', '<leader>gb', '<cmd>Git blame<CR>',
+      { noremap = true, silent = true, desc = "Fugitive: Git blame" })
+    vim.api.nvim_set_keymap('n', '<leader>gi', '<cmd>Git diff<CR>',
+      { noremap = true, silent = true, desc = "Fugitive: Inline git diff" })
   end
-} 
+}
