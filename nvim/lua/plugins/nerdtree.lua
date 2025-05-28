0b1da25e084cf469646cf0025bb6240abaa80738
Divan Visagie <me@divanv.com>
2025-05-29 01:32:52 +0200

    Bind tree collapse in vim to command + b, to mirror other apps

--- nvim/lua/plugins/nerdtree.lua
+++ nvim/lua/plugins/nerdtree.lua
@@ -6,5 +6,6 @@ return {
     vim.api.nvim_set_keymap('n', '<leader>nt', '<cmd>NERDTreeToggle<CR>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('n', '<leader>nf', '<cmd>NERDTreeFind<CR>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('n', '<leader>haf', '<cmd>NERDTreeFind<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<D-b>', '<cmd>NERDTreeToggle<CR>', { noremap = true, silent = true })
   end
-} 
+}
